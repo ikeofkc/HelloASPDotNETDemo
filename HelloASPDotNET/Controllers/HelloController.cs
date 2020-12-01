a8fd1803a1141ae034aac55bd3996247075eec52
Ike Ah-Loe <ikeahloe@gmail.com>
2020-12-01 10:03:24 -0600

    Added basic controller and welcome action method with default value if not given one through query

--- HelloASPDotNET/Controllers/HelloController.cs
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -12,5 +12,11 @@ namespace HelloASPDotNET.Controllers
             string html = "<h1>Hello World!</h1>";
             return Content(html, "text/html");
         }
+
+        public IActionResult Welcome(string name = "Ike")
+        {
+            return Content($"<h1>Test!! Your name is {name}!</h1>", "text/html");
+        }
+        
     }
 }
