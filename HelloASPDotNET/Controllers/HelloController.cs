c4c9097512cd9cd3650ad65f89e363f782ee5ff6
Ike Ah-Loe <ikeahloe@gmail.com>
2020-12-01 10:17:29 -0600

    messing around more with action methods and some if statements

--- HelloASPDotNET/Controllers/HelloController.cs
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -6,16 +6,27 @@ namespace HelloASPDotNET.Controllers
     {
         // GET /<controller>/
         [HttpGet]
-        // [Route("/helloworld/")]
+        [Route("/helloworld/")]
         public IActionResult Index()
         {
             string html = "<h1>Hello World!</h1>";
             return Content(html, "text/html");
         }
 
+
+        [HttpGet]
+        [Route("random/welcome/{name?}")]
         public IActionResult Welcome(string name = "Ike")
         {
+            if (name.ToLower() == "ike")    
+            {
             return Content($"<h1>Test!! Your name is {name}!</h1>", "text/html");
+            }
+            else
+            {
+                return Content("<h1>You're a liar!<h1>", "text/html");
+            }
+                
         }
         
     }
